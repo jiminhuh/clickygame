4c80d2d3a8d4a6f14edf5f4e1cfb54b4e9e9d04c
Jimin Huh <jiminhuh95@gmail.com>
2019-05-03 23:37:27 -0500

    Updates

---
+++ precache-manifest.04ea23b34c6e86e05be6250a671c412a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "6c69e22f6d7932e6e8f8",
+    "url": "/clickygame/static/js/main.25e5a559.chunk.js"
+  },
+  {
+    "revision": "abaade4e91dd7ea4842e",
+    "url": "/clickygame/static/js/2.8427f788.chunk.js"
+  },
+  {
+    "revision": "6c69e22f6d7932e6e8f8",
+    "url": "/clickygame/static/css/main.47c5bc98.chunk.css"
+  },
+  {
+    "revision": "abaade4e91dd7ea4842e",
+    "url": "/clickygame/static/css/2.4ea8a8c5.chunk.css"
+  },
+  {
+    "revision": "cfbcf2a9a3a946ac0c648cd2876b89f0",
+    "url": "/clickygame/index.html"
+  }
+];
